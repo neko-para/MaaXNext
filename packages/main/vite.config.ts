2f2e88341ff9a9fd786847e7e3dd243d4300d133
nekosu <liao.hy@outlook.com>
2023-09-06 01:53:30 +0800

    feat: deal with koffi

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from 'vite'
 import { builtinModules } from 'module'
-import { resolve } from 'path'
-import native from 'vite-plugin-native'
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [
-    native({
-      outDir: '../../dist/main',
-      map(mapping) {
-        if (mapping.id === 'koffi.node') {
-          const m = /\/([^/]+)\/koffi.node$/.exec(mapping.native)
-          if (m) {
-            mapping.output = mapping.output.replace('koffi.node', `${m[1]}.node`)
-            // mapping.id = `./${m[1]}.node`
-            mapping.id = `./darwin_x64.node`
-          }
-        }
-        return mapping
-      },
-      target: 'cjs'
-    })
-  ],
   root: __dirname,
   build: {
     lib: {
@@ -31,9 +12,9 @@ export default defineConfig({
       fileName: () => '[name].js'
     },
     outDir: '../../dist/main',
-    // emptyOutDir: true,
+    emptyOutDir: true,
     rollupOptions: {
-      external: ['electron', ...builtinModules]
+      external: ['electron', ...builtinModules, 'koffi']
     },
     commonjsOptions: {
       ignoreDynamicRequires: true
